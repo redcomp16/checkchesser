714f1c18e437b2254c84a9ddb7cb7f08c3098d24
redcomp16 <hexadecimalredstonecomputer@gmail.com>
2026-01-02 00:17:25 -0500

    Add CORS allowed origins for CheckChesser

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,6 +60,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://redcomp16.github.io"
+    "https://www.checkchesser.com",
+    "https://checkchesser.com",
 ]
 
 TEMPLATES = [
