00272902bdfa8675f07567070624630760a9fe69
redcomp16 <hexadecimalredstonecomputer@gmail.com>
2026-01-02 01:05:53 -0500

    fix settings formatting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'backend.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://redcomp16.github.io"
+    "https://redcomp16.github.io",
     "https://www.checkchesser.com",
     "https://checkchesser.com",
     "http://www.checkchesser.com",
